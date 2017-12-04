97e2893a8b90e439ef689b351b6f49712a3c5ead
Paul Valle <paul.valle@datadisk.co.uk>
2017-12-04 15:28:17 +0000

    got user create and list test working

--- test/mongo_api/user_create_test.js
+++ test/mongo_api/user_create_test.js
@@ -8,7 +8,7 @@ describe('API User test', () => {
 
   it.only('can create a User', (done) => {
 
-    let body = {
+    let user = {
       email: 'paul.valle@datadisk.co.uk',
       password: 'password',
       role: 'admin'
@@ -16,21 +16,36 @@ describe('API User test', () => {
 
     server
       .post("/ddmovie_api/users")
-      .send(body)
+      .send(user)
+      .expect(201)      // if anything other than 201 'err' will be populated
       .end(function(err, res) {
-        //console.log('Res ' + res);
-      })
 
-    server
-      .get("/ddmovie_api/users")
-      .end(function(err, res) {
-        //console.log('Body: ' + res.body[0].email);
-        assert.equal(res.body[0].email, 'paul.valle@datadisk.co.uk');
-        assert.equal(res.body[0].password, 'password');
-        assert.equal(res.body[0].role, 'admin');
-        done();
+        // check for any errors post request errors
+        if (err) {
+          console.log('Error: ' + err);
+          done(err)       // stop the test if we have an error
+        }
+
+        // check that we got back a 201
+        assert.equal(res.statusCode, 201);
+
+        // check the user exists in mongo
+        server
+          .get("/ddmovie_api/users")
+          .end(function (err, res) {
+
+            // if we get any errors abort
+            if (err) {
+              console.log('Error: ' + err);
+              done(err)
+            }
+
+            // check that user has been retrieved
+            assert.equal(res.body[0].email, 'paul.valle@datadisk.co.uk');
+            assert.equal(res.body[0].password, 'password');
+            assert.equal(res.body[0].role, 'admin');
+            done();
+          });
       });
-
   });
-
 });
