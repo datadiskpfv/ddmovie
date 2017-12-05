9d121ad5769aa3dd7d2256701c765652c28afc91
Paul Valle <paul.valle@datadisk.co.uk>
2017-12-05 14:12:42 +0000

    finished the user CRUD controller methods and mocha tests

--- test/mongo_api/user/user_update_test.js
+++ test/mongo_api/user/user_update_test.js
@@ -13,7 +13,10 @@ describe('API User update test', () => {
       .end(function (err, res) {done();});
   });
 
-  it('can update a specific user', (done) => {
+  it.only('can update a specific user', (done) => {
+
+    // because its all async we have to nest each test to make sure
+    // that the task has completed before moving onto the next task
 
     server
       .get("/ddmovie_api/users/email/paul.valle@datadisk.co.uk")
@@ -22,19 +25,22 @@ describe('API User update test', () => {
         //console.log('User Count (before): ' + res.body.length);
         assert.equal(res.body.length, 1);
 
-        // Now we delete the user from the Mongo DB
+        // Now we change the data
         const userData = {
           email: 'paul.valley@datadisk.co.uk',
           password: 'update-password',
-          role: 'user'
+          role: 'user',
+          accountActive: res.body.accountActive,
+          createdOn: res.body.createdOn
         }
 
+        // save the changed user data to Mongo DB
         server
           .put("/ddmovie_api/users/" + res.body[0]._id)
           .send(userData)
           .end(function(err, res) {
 
-            // Now we check the user has been deleted
+            // Now we check the user data has changed
             // make sure we search for the new name
             server
               .get("/ddmovie_api/users/email/paul.valley@datadisk.co.uk")
@@ -47,7 +53,6 @@ describe('API User update test', () => {
               })
           })
       })
-
   });
 
 });
